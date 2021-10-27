192f64cb51d53bcecc46498c96d6e71264feb253
Fabio Augusto Mazuchi <mazuchi.augusto@hotmail.com>
2021-10-27 15:38:39 -0300

    adiciona requisito 4 e 5

--- script.js
+++ script.js
@@ -4,10 +4,15 @@ const p = document.querySelector('#carta-gerada');
 
 btn.addEventListener('click', function () {
     let array = input.value.split(' ');
+    p.innerHTML = '';
 
-    for (let a of array) {
-        let span = document.createElement('span');
-        span.innerHTML = a;
-        p.appendChild(span);
+    if (input.value === '' || input.value === ' ') {
+        p.innerHTML = 'Por favor, digite o conteúdo da carta.';
+    } else {
+        for (let a of array) {
+            let span = document.createElement('span');
+            span.innerHTML = a;
+            p.appendChild(span);
+        }
     }
 });
